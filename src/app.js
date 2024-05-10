cfe7b5a70cd888535dc0f1cd4723f80fe6e00cc5
Mehran Khodaei <khodaei.dev@gmail.com>
2024-05-10 09:50:35 +0330

    Dev app.js file And set statics folders & ejs template Engin

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+
+// Body Parser
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+
+// static Folder
+app.use("/views", express.static(path.join(__dirname, "views")));
+app.use("/css", express.static(path.join(__dirname, "./../public/css")));
+app.use("/js", express.static(path.join(__dirname, "./../public/js")));
+app.use("/fonts", express.static(path.join(__dirname, "./../public/fonts")));
+app.use("/images", express.static(path.join(__dirname, "./../public/images")));
+
+// template Engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// 404 Error handler
+app.use((req, res) => {
+  console.log("this is Path Not Found =>", req.path);
+  res.status(404).json({ message: "Page Not found" });
+});
+module.exports = app;
