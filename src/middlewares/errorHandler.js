d22cb63762f1f25fd7ebc52d5cab585ff93c5710
khodaeidev <khodaei.dev@gmail.com>
2024-06-13 20:50:16 +0330

    Dev 404 err handller

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,6 +2,9 @@ exports.errorHandler = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || "error";
 
+  if (error.statusCode === 404) {
+    return res.status(error.statusCode).render("404err/index");
+  }
   req.flash("error", error.message);
   return res.status(res.statusCode).redirect("back");
 };
