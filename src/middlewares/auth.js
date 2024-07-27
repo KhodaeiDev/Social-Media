0bae4dad97a045234083c618591f8867d690dd14
khodaeidev <khodaei.dev@gmail.com>
2024-07-27 14:06:36 +0330

    Dev Some Edit in views

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,20 +5,20 @@ module.exports = async (req, res, next) => {
   try {
     const token = req.cookies["access-token"];
     if (!token) {
-      req.flash("error", "Please Login First");
+      req.flash("error", "Please Login or Register First");
       return res.redirect("/auth/login");
     }
 
     const decode = await jwt.verify(token, process.env.JWT_ACCESS_SECRET);
     if (!decode) {
-      req.flash("error", "Please Login First");
+      req.flash("error", "Please Login or Register First");
       return res.redirect("/auth/login");
     }
 
     const userId = decode.userId;
     const user = await userModel.findOne({ _id: userId });
     if (!user) {
-      req.flash("error", "Please Login First");
+      req.flash("error", "Please Login or Register First");
       return res.redirect("/auth/login");
     }
 
